6e3833aa4f04d86e34ed2b150b67c8343cbcb34b
eliebeaino <eliebeaino89@gmail.com>
2020-06-08 17:58:38 +0300

    pathfinding 2
    - removed pathing
    - dictionary for waypoints in pathfindind.cs
    - color end/start cube
    - look for neighbouring blocks and color them

--- Tower Defense/Assets/Waypoint.cs
+++ Tower Defense/Assets/Waypoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    Vector2Int gridPos;
+    //Vector2Int gridPos;
     const int gridSize = 10;
 
 
@@ -14,11 +14,17 @@ public class Waypoint : MonoBehaviour
         return gridSize;
     }
 
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
         return new Vector2Int(
-        Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
-        Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+        Mathf.RoundToInt(transform.position.x / gridSize),
+        Mathf.RoundToInt(transform.position.z / gridSize)
         );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
     }
 }

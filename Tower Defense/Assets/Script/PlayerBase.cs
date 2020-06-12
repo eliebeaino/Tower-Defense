a74ee1419ddecadd300824e147282e63dbcd467a
eliebeaino <eliebeaino89@gmail.com>
2020-06-12 20:03:54 +0300

    added UI
    - Added UI (base health - enemies alive)
    - fixed bug with untargateable enemies)

--- Tower Defense/Assets/Script/PlayerBase.cs
+++ Tower Defense/Assets/Script/PlayerBase.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerBase : MonoBehaviour
 {
     public float baseHealth = 10;
+    [SerializeField] TextMeshProUGUI baseHealthText;
+    [SerializeField] TextMeshProUGUI EnemiesAliveText;
 
+    private void Start()
+    {
+        baseHealthText.text = baseHealth.ToString();
+    }
+
+    private void Update()
+    {
+        EnemiesAlive();
+    }
+
+    // reduce health based on damage taken
     public void ProcessDmg(float damagetaken)
     {
         baseHealth -= damagetaken;
+        baseHealthText.text = baseHealth.ToString();
     }
+
+    public void EnemiesAlive()
+    {
+        var sceneEnemies = FindObjectsOfType<Enemy>();
+        EnemiesAliveText.text = sceneEnemies.Length.ToString();
+    }    
 }
